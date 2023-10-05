089e97bd20ca1812fc78bebec3395f44c8ddb3f7
evschankin <evschankin555@gmail.com>
2023-10-05 23:44:46 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://www.maturmarket.ru/');
+define('HTTP_CATALOG', 'https://www.maturmarket.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.maturmarket.ru/');
+define('HTTPS_CATALOG', 'https://www.maturmarket.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/maturmarket/catalog/');
+define('DIR_SYSTEM', '/var/www/maturmarket/system/');
+define('DIR_IMAGE', '/var/www/maturmarket/image/');
+define('DIR_STORAGE', '/var/www/maturmarket_storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'maturmarket');
+define('DB_PASSWORD', 'gfdhgJG_U49578-0-3J');
+define('DB_DATABASE', 'maturmarket');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
